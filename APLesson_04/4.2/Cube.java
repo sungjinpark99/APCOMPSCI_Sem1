92ffdb4ed5f3e291405427303a179327baad3f65
sungjinpark99 <sungjinpark2110@gmail.com>
2016-10-04 08:37:25 -0700

    edited lab

--- APLesson_04/4.2/Cube.java
+++ APLesson_04/4.2/Cube.java
@@ -5,11 +5,11 @@ public class Cube
 	static double side;
 	static double sa;
 	
-	public class void main(String[]args)
+	public static void main(String[]args)
 	{
 		Scanner kb = new Scanner(System.in);
 		
-		System.out.println("How long is the side");
+		System.out.println("How long is the side?");
 		side = kb.nextDouble();
 		
 		calcSurf();
@@ -23,6 +23,6 @@ public class Cube
 	
 	public static void print()
 	{
-		System.out.printf("The surface area of the cube is %.5f")
+		System.out.printf("The surface area of the cube is %.5f", sa);
 	}
 }
