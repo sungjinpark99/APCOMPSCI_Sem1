992d0b9995c4bc1f61a589b36f9f4bb55d531eba
sungjinpark99 <sungjinpark2110@gmail.com>
2016-11-16 09:51:22 -0800

    edits

--- APLesson_06/Lab_06.1/Song.java
+++ APLesson_06/Lab_06.1/Song.java
@@ -4,14 +4,14 @@ public class Song
 {
 	public static void main(String[]args)
 	{
-		sing("Na",4);
-		sing("Na",4);
-		sing("Hey",3);
-		sing("Goodbye!",1);
+		sing("Na",3);
+		sing("Na",3);
+		sing("Hey",2);
+		sing("Goodbye!",0);
 	}
 	public static void sing(String word, int num)
 	{
-		for(int i = 1; i<=num; i++)
+		for(int i = 0; i<=num; i++)
 		{
 			System.out.printf(word + " ");
 		}
